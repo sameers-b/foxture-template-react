5ca2d5ada6beccfbb43c2fc6337acf0ab0db12d2
Sameer Singh <sameersingh7458@gmail.com>
2021-11-01 11:39:23 +0530

    product-done

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,16 +1,37 @@
 import "./Product.css";
+import GradeIcon from "@material-ui/icons/Grade";
 
 const Product = () => {
   return (
     <>
       <div className="product">
         <div className="product-card">
+          <span className="product-offer">NEW</span>
           <p></p>
           <div className="product-icon">
             <span></span>
             <span></span>
             <span></span>
             <span></span>
+          </div>
+        </div>
+        <div className="product-details">
+          <div className="product-review">
+            <GradeIcon color="secondary" fontSize="small" />
+            <GradeIcon color="secondary" fontSize="small" />
+            <GradeIcon color="secondary" fontSize="small" />
+            <GradeIcon color="secondary" fontSize="small" />
+            <GradeIcon color="secondary" fontSize="small" />
+            <span className="product-total-review"> ( 32 reviews )</span>
+          </div>
+          <div className="product-name">Pendent light roof minimal</div>
+          <div className="product-price">
+            <span>$ 55.00</span>
+          </div>
+          <div className="product-color">
+            <span style={{ "background-color": "red" }}></span>
+            <span style={{ "background-color": "black" }}></span>
+            <span style={{ "background-color": "pink" }}></span>
           </div>
         </div>
       </div>
